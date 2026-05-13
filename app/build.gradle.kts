93025a9dda8d61db53e44d0d9c729e375d6f079a
skj <skj@gmail.com>
2026-05-13 23:49:38 +0530

    Added AppCompat dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.appcompat)
 
     // Compose BOM
     implementation(platform(libs.androidx.compose.bom))
